b20931735d44c846e2829f910023dbd90e6874e2
gaoshine <gaoshine2008@gmail.com>
2016-02-18 12:58:24 +0800

    README.md 修改

--- gps.py
+++ gps.py
@@ -2,12 +2,12 @@
 # GPS服务器端代码,负责接收GPS设备上报的数据和下发命令指令,负责接收GPS上传信息并整理后在发送给指定的API1
 
 import time, socket, threading
-#导入JSON
+# 导入JSON
 import json
-import  string
-#导入re模块
+import string
+# 导入re模块
 import re
-from jsonpost import jsonpost
+from jsonpost import jsonpost, baidugps
 
 
 def tcplink(sock, addr):
@@ -17,57 +17,69 @@ def tcplink(sock, addr):
         data = sock.recv(1024)
         time.sleep(1)
         jsonpost(decodegps(data))
-        mlog('gps',data)
+        mlog('gps', data)
         print 'Rev:%s' % data
         if data == 'exit' or not data:
             break
-	    sock.send('OK')
+            sock.send('OK')
     sock.close()
     print 'Connection from %s:%s closed.' % addr
 
-def mlog(TAG,txt):
-    f = open('gps.log','a')
+
+def mlog(TAG, txt):
+    f = open('gps.log', 'a')
     localtime = time.asctime(time.localtime(time.time()))
     f.write(TAG + '/' + localtime + '/' + txt)
     f.close()
 
-def decodegps( mGPS ):
-    pattern = re.compile('MG201(\d{15}),AB&A(\d{6})(\d{8})(\d{9})([0-9])(\d{2})(\d{2})(\d{6})',re.IGNORECASE)
 
+def decodegps(mGPS):
+    pattern = re.compile('MG201(\d{15}),AB&A(\d{6})(\d{8})(\d{9})([0-9])(\d{2})(\d{2})(\d{6})', re.IGNORECASE)
+    x = 0.0
+    y = 0.0
     info = {}
-    items = re.findall(pattern,mGPS)
+    items = re.findall(pattern, mGPS)
     for item in items:
-        print 'IMEI:' + item[0],'Time:' +  item[1] ,'lat:' + item[2] ,'lon:' +item[3],item[4],item[5],item[6],'Date:' +item[7]
+        print 'IMEI:' + item[0], 'Time:' + item[1], 'lat:' + item[2], 'lon:' + item[3], item[4], item[5], item[
+            6], 'Date:' + item[7]
 
         lon = item[3]
-        lon = string.atof(lon[0:3]) + string.atof(lon[3:5])/60 + string.atof(lon[5:9])/600000
+        lon = string.atof(lon[0:3]) + string.atof(lon[3:5]) / 60 + string.atof(lon[5:9]) / 600000
         lat = item[2]
-        lat = string.atof(lat[0:2]) + string.atof(lat[2:4])/60 + string.atof(lat[4:8])/600000
+        lat = string.atof(lat[0:2]) + string.atof(lat[2:4]) / 60 + string.atof(lat[4:8]) / 600000
 
-        mdate =  item[7]
-        mdate = '20' +mdate[4:6] + '-' +  mdate[2:4] + '-' +  mdate[0:2]
+        mdate = item[7]
+        mdate = '20' + mdate[4:6] + '-' + mdate[2:4] + '-' + mdate[0:2]
         mtime = item[1]
         mtime = mtime[0:2] + ':' + mtime[2:4] + ':' + mtime[4:6]
-
-        info['lat'] =  "%.6f" % lat
-        info['lon'] =  "%.6f" % lon
+        x = lon
+        y = lat
+        info['lat'] = "%.6f" % lat
+        info['lon'] = "%.6f" % lon
         info['time'] = mtime
         info['date'] = mdate
         info['IMEI'] = item[0]
         break
-        #print items
+    if (x + y) < 1:
+        return
+    d = baidugps(x, y)
+    print d
+    print d[0]['x'], d[0]['y']
+    info['lon'] = d[0]['x']
+    info['lat'] = d[0]['y']
 
-    pattern = re.compile('&M(\d{3})',re.S)
-    items = re.findall(pattern,mGPS)
+    # print items
+
+    pattern = re.compile('&M(\d{3})', re.S)
+    items = re.findall(pattern, mGPS)
     for item in items:
-        #print item[0]+item[1] +'.'+item[2]
-        info['battery'] = item[0]+item[1] +'.'+item[2]
+        # print item[0]+item[1] +'.'+item[2]
+        info['battery'] = item[0] + item[1] + '.' + item[2]
         break
 
     jsonStr = json.dumps(info)
+    # print jsonStr
     return jsonStr
-
-
 
 
 if __name__ == '__main__':
@@ -82,6 +94,3 @@ if __name__ == '__main__':
         # 创建新线程来处理TCP连接:
         t = threading.Thread(target=tcplink, args=(sock, addr))
         t.start()
-
-
-
